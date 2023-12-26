53b7bbcb606252438207192d616d56ce0abdaff5
kek145 <yurabogomolov2004@gmail.com>
2023-12-26 21:46:36 +0200

    UPDATE | Init project and added ProductController and endpoints

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductApi.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductController : ControllerBase
+{
+
+    [HttpPost]
+    [Route("create")]
+    public async Task<IActionResult> CreateProduct()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllProducts()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("{productId:int}")]
+    public async Task<IActionResult> GetProductById(int productId)
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+    [Route("{productId:int}/update")]
+    public async Task<IActionResult> UpdateProduct(int productId)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [Route("{productId:int}/delete")]
+    public async Task<IActionResult> DeleteProduct(int productId)
+    {
+        return NoContent();
+    }
+}
